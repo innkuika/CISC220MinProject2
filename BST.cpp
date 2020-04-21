42f6aef13e701341304511602a4bbf2b7085fb4a
innkuika <innkuika@gmail.com>
2020-04-21 08:44:11 -0400

    BST initial commit

--- BST.cpp
+++ BST.cpp
@@ -187,3 +187,66 @@ void setHeight(TNode *n);
 // 	}
 // 	return n;
 // }
+
+BST::BST() {
+	root = NULL;
+}
+BST::BST(string s) {
+	root = new TNode(s);
+}
+
+void BST::printTreeIO() {
+	if (root == NULL ) {
+		cout << "Empty Tree" << endl;
+	}
+	else {
+		cout << endl<<"Printing In Order:" <<endl;
+		printTreeIO(root);
+	}
+}
+
+
+void BST::printTreePre() {
+	if (root == NULL ) {
+		cout << "Empty Tree" << endl;
+	}
+	else {
+		cout << endl<<"Printing PreOrder:" <<endl;
+		printTreePre(root);
+	}
+}
+
+
+void BST::printTreePost() {
+	if (root == NULL ) {
+		cout << "Empty Tree" << endl;
+	}
+	else {
+		cout << endl<<"Printing PostOrder:" <<endl;
+		printTreePost(root);
+	}
+}
+
+
+void BST::clearTree() {
+	if (root == NULL) {
+		cout << "Tree already empty" << endl;
+	}
+	else {
+		cout << endl << "Clearing Tree:" << endl;
+		clearTree(root);
+		root = NULL;
+	}
+}
+void BST::clearTree(TNode *tmp) {
+	if (tmp == NULL) {
+		return;
+	}
+	else {
+		clearTree(tmp->left);
+		clearTree(tmp->right);
+		tmp->printNode();
+		delete(tmp);
+	}
+}
+
